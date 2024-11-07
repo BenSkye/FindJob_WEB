b884d44a0f5678b6c0126cbe818f6ac94b34ab09
luan0811 <nguyentranminhluan9a10@gmail.com>
2024-11-07 17:04:28 +0700

    bao ve trang admin

---
+++ admin_frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+import Forbidden from './common/403';
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const auth = useContext(AuthContext);
+    const location = useLocation();
+
+    if (auth?.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!auth?.isAuthenticated) {
+        return <Navigate to="/admin/login" state={{ from: location }} replace />;
+    }
+
+    if (!auth?.checkPermission()) {
+        return <Forbidden />;
+    }
+
+    return <>{children}</>;
+};
+
+export default ProtectedRoute; 
