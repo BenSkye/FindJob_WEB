3d8d78ad9bfa98c8b412533a3bde396990f4efc7
laptop-m8du3f9f <quangtran214203@gmail.com>
2024-11-09 21:34:19 +0700

    1

--- frontend/src/components/candidate/CustomCKEditor.tsx
+++ frontend/src/components/candidate/CustomCKEditor.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import React, { Suspense } from 'react';
+const CKEditor = React.lazy(() => import('@ckeditor/ckeditor5-react').then(module => ({
+    default: module.CKEditor
+})));
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 interface CustomCKEditorProps {
@@ -16,31 +18,44 @@ const CustomCKEditor: React.FC<CustomCKEditorProps> = ({
     height = '200px'
 }) => {
     return (
-        <div style={{ minHeight: height }}>
-            <CKEditor
-                editor={ClassicEditor}
-                data={data}
-                config={{
-                    toolbar: [
-                        'heading',
-                        '|',
-                        'bold',
-                        'italic',
-                        'link',
-                        'bulletedList',
-                        'numberedList',
-                        '|',
-                        'undo',
-                        'redo'
-                    ],
-                    placeholder: placeholder,
-                }}
-                onChange={(event: any, editor: any) => {
-                    const data = editor.getData();
-                    onChange(data);
-                }}
-            />
-        </div>
+        <Suspense fallback={
+            <div style={{
+                height,
+                border: '1px solid #d9d9d9',
+                borderRadius: '2px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                Đang tải editor...
+            </div>
+        }>
+            <div style={{ minHeight: height }}>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data={data}
+                    config={{
+                        toolbar: [
+                            'heading',
+                            '|',
+                            'bold',
+                            'italic',
+                            'link',
+                            'bulletedList',
+                            'numberedList',
+                            '|',
+                            'undo',
+                            'redo'
+                        ],
+                        placeholder: placeholder,
+                    }}
+                    onChange={(event: any, editor: any) => {
+                        const data = editor.getData();
+                        onChange(data);
+                    }}
+                />
+            </div>
+        </Suspense>
     );
 };
 
